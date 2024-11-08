58ee6a7e2a7aff7c8cf434946f1fb824ec35637a
antonkkk <anton.kuxo@gmail.com>
2024-11-08 23:12:33 +0300

    tests ready

---
+++ conftest.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.chrome.options import  Options
+import pytest
+from selenium import webdriver
+
+@pytest.fixture()
+def driver():   #browser
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
+    driver.maximize_window()
+    driver.implicitly_wait(3)
+    yield driver
+    driver.close()
